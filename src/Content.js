4526d7005f8c6703280005b6d4db1d027b49b255
Ghouma Mohamed <ghoumamed002@gmail.com>
2023-05-16 20:47:06 +0100

    Fix build generation

--- src/Content.js
+++ src/Content.js
@@ -11,14 +11,14 @@ import symfony from "./assets/images/Skills/symfony.svg";
 import prestashop from "./assets/images/Skills/prestashop.png";
 import git from "./assets/images/Skills/git.png";
 
-import services_logo1 from "./assets/images/Services/logo1.png";
-import services_logo2 from "./assets/images/Services/logo2.png";
-import services_logo3 from "./assets/images/Services/logo3.png";
+// import services_logo1 from "./assets/images/Services/logo1.png";
+// import services_logo2 from "./assets/images/Services/logo2.png";
+// import services_logo3 from "./assets/images/Services/logo3.png";
 
-import project1 from "./assets/images/projects/img1.png";
-import project2 from "./assets/images/projects/img2.png";
-import project3 from "./assets/images/projects/img3.png";
-import person_project from "./assets/images/projects/person.png";
+// import project1 from "./assets/images/projects/img1.png";
+// import project2 from "./assets/images/projects/img2.png";
+// import project3 from "./assets/images/projects/img3.png";
+// import person_project from "./assets/images/projects/person.png";
 
 import avatar1 from "./assets/images/Testimonials/avatar1.png";
 import avatar2 from "./assets/images/Testimonials/avatar2.png";
@@ -160,25 +160,25 @@ export const content = {
       },
     ],
   },
-  Projects: {
-    title: "Projects",
-    subtitle: "MY CREATION",
-    image: person_project,
-    project_content: [
-      {
-        title: "Gym Website",
-        image: project1,
-      },
-      {
-        title: "Social Media web",
-        image: project2,
-      },
-      {
-        title: "Creative Website",
-        image: project3,
-      },
-    ],
-  },
+  // Projects: {
+  //   title: "Projects",
+  //   subtitle: "MY CREATION",
+  //   image: person_project,
+  //   project_content: [
+  //     {
+  //       title: "Gym Website",
+  //       image: project1,
+  //     },
+  //     {
+  //       title: "Social Media web",
+  //       image: project2,
+  //     },
+  //     {
+  //       title: "Creative Website",
+  //       image: project3,
+  //     },
+  //   ],
+  // },
   Testimonials: {
     title: "Testimonials",
     subtitle: "MY CLIENT REVIEWS",
