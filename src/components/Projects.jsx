4e950576ec75ca94ed87609c461cc3213ac0e216
Ghouma Mohamed <ghoumamed002@gmail.com>
2026-05-04 22:06:37 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 
-import { Pagination } from "swiper";
+import { Pagination } from "swiper/modules";
 
 const Projects = () => {
   const { Projects } = content;
