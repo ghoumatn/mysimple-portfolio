85653d7d591d67ae1ad871364c459e1b6c4a41de
Ghouma Mohamed <ghoumamed002@gmail.com>
2026-05-04 22:06:19 +0100

    Update Swiper import path for Pagination component

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,7 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 
-import { Pagination } from "swiper";
+import { Pagination } from "swiper/modules";
 import { useState } from "react";
 
 const Testimonials = () => {
